4436d6a0f1806b040a5805f2690409a56d22485c
Thomas Fini Hansen <xen@xen.dk>
2021-01-19 13:42:11 +0100

    Inital commit

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+STATE=error
+
+if backstop $1; then
+  STATE=success
+fi
+
+if [ "$1" == "test" ]; then
+    echo "$BACKSTORE_KEY" > /tmp/key
+    chmod go= /tmp/key
+
+    REFERENCE_IMAGES=$(jq -r .paths.bitmaps_reference backstop.json)
+    TEST_IMAGES=$(jq -r .paths.bitmaps_test backstop.json)
+    HTML_REPORT=$(jq -r .paths.html_report backstop.json)
+    SHA=$(echo $GITHUB_REPOSITORY:$HEAD_SHA | sha1sum - | cut -d" " -f 1)
+
+    rsync -e 'ssh -i /tmp/key -p 1984 -o StrictHostKeyChecking=no' -r ${REFERENCE_IMAGES} ${TEST_IMAGES} ${HTML_REPORT} store@backstore.reload.dk:${SHA}/
+
+    BACKSTORE_URL=https://backstore.reload.dk/${SHA}/$(basename $HTML_REPORT)
+
+    GITHUB_API_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${HEAD_SHA}"
+    STATUS_DATA="{\"state\": \"${STATE}\", \"description\": \"Report\", \"context\": \"Visual regression test\", \"target_url\": \"${BACKSTORE_URL}\"}"
+    curl --fail --silent -X POST --user ":${GITHUB_TOKEN}" "${GITHUB_API_URL}" --data "${STATUS_DATA}"
+fi
+
+if [ "$STATE" == "success" ]; then
+  exit
+fi
+
+exit 1
